03ecc4f15dfebb377ae1c9ce9b63989f93f4dac1
claudiopjfilho <claudiopjfilhodev@gmail.com>
2024-01-29 20:54:49 -0300

    add css

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,4 +1,11 @@
-import "./ogo.css";
+import "./Logo.css";
 import React from "react";
+import logo from "../../assets/images/logo.svg";
 
-export default props => <aside className="logo">Logo</aside>;
+export default props => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
