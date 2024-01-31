0ccc288af26de7cd6302eb45006d853638c4e31b
claudiopjfilho <claudiopjfilhodev@gmail.com>
2024-01-30 21:32:23 -0300

    add crud

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,4 +1,16 @@
 import "./Nav.css";
 import React from "react";
 
-export default props => <aside className="menu-area">Navegação</aside>;
+export default props => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className={`fa fa-${props.icon}`}>Inicio</i>
+      </a>
+
+      <a href="#/users">
+        <i className={`fa fa-${props.users}`}>{props.usuarios}</i>
+      </a>
+    </nav>
+  </aside>
+);
