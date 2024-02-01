02801a1b7ad5560d6e29e8729919f075aaf66017
claudiopjfilho <claudiopjfilhodev@gmail.com>
2024-01-31 22:37:33 -0300

    cru

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,17 @@
 import "./Nav.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default props => (
   <aside className="menu-area">
     <nav className="menu">
-      <a href="#/">
+      <Link to="/">
         <i className={`fa fa-${props.icon}`}>Inicio</i>
-      </a>
+      </Link>
 
-      <a href="#/users">
+      <Link to="/users">
         <i className={`fa fa-${props.users}`}>{props.usuarios}</i>
-      </a>
+      </Link>
     </nav>
   </aside>
 );
